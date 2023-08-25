e4de598f1df9633844a6f2b1e70c6f73775d29c0
Shazoji90 <squash7@live.com>
2023-08-25 19:51:15 +0700

    Fetch API dengan useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,9 @@ import Article from "../components/Article";
 import Search from "../components/Search";
 
 function Homepage() {
-  // const [posts, setPosts] = useState([]);
   const [posts, setPosts] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [externalPosts, setExternalPosts] = useState([]);
 
   const searchValue = (value) => {
     const lowercasedValue = value.toLowerCase();
@@ -17,29 +17,11 @@ function Homepage() {
     setTotalPosts(filteredPosts.length);
   };
 
-  // React versi lama menggunakan 3 lifecycle
-  // componentDidMount -> komponen yg dijalankan ketika halaman pertamakali di-render
-  // componentDidUpdate -> ketika state berubah
-  // componentWillUnmount -> ketika komponen akan dibuang/ditutup
-
   useEffect(() => {
-    // console.log("render");  -> akan muncul 2 kata render di console
-    // karena pada file main.jsx ada StrictMode yg nantinya tidak dipakai dalam production
-    console.log("render");
-
-    // Ini merupakan Unmount, biasanya digunakan untuk membersihkan side effect yg terjadi
-    // misalkan kita menghapus eventlister saat component ditutup agar tidak berjalan terus
-    // atau untuk menghapus setInterval agar tidak berjalan terus meski component ditutup
-    // agar aplikasi tidak berat
-    return () => {
-      // removeEventListener;
-      // clearInterval;
-      console.log("cleanup");
-    };
-  }, [posts]);
-  {
-    /* -> array [] kosong untuk menghentikan render */
-  }
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setExternalPosts(json));
+  }, []);
 
   return (
     <>
@@ -47,6 +29,13 @@ function Homepage() {
       <Search onSearchChange={searchValue} totalPosts={totalPosts} />
       {posts.map((props, index) => (
         <Article {...props} key={index} />
+      ))}
+      <hr />
+      <h2>External Posts</h2>
+      {/* {externalPosts.map((item, index) => ( */}
+      {externalPosts.map((item) => (
+        // <div key={index}>- {item.title}</div>
+        <div key={item.id}>- {item.title}</div>
       ))}
     </>
   );
