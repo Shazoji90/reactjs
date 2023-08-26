0207a1ff51beea793e88fe5f93cac5ce477351e0
Shazoji90 <squash7@live.com>
2023-08-26 16:44:36 +0700

    Halaman Blog

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,6 @@ import Search from "../components/Search";
 function Homepage() {
   const [posts, setPosts] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
-  const [externalPosts, setExternalPosts] = useState([]);
 
   const searchValue = (value) => {
     const lowercasedValue = value.toLowerCase();
@@ -17,35 +16,12 @@ function Homepage() {
     setTotalPosts(filteredPosts.length);
   };
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => setExternalPosts(json));
-  }, []);
-
-  // gunakan multiple useEffect jika tidak ingin melakukan fetching API di atas
-  useEffect(() => {
-    console.log("ada post baru");
-  }, [posts]);
-
-  // gunakan jika butuh tiap kali ada perubahan/rendering
-  useEffect(() => {
-    console.log("render terus");
-  });
-
   return (
     <>
       <h1>Simple Blog</h1>
       <Search onSearchChange={searchValue} totalPosts={totalPosts} />
       {posts.map((props, index) => (
         <Article {...props} key={index} />
-      ))}
-      <hr />
-      <h2>External Posts</h2>
-      {/* {externalPosts.map((item, index) => ( */}
-      {externalPosts.map((item) => (
-        // <div key={index}>- {item.title}</div>
-        <div key={item.id}>- {item.title}</div>
       ))}
     </>
   );
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 
 import Home from "../pages";
+import Blog from "../pages/blogs";
 import About from "../pages/About";
 
 export const router = createBrowserRouter([
@@ -15,13 +16,13 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
         path: "/about",
         element: <About />,
       },
     ],
-  },
-  {
-    path: "/about",
-    element: <About />,
   },
 ]);
