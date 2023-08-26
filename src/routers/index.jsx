ca5ed0cde8e8b7ec2413ef454d386aa2179cb8e9
Shazoji90 <squash7@live.com>
2023-08-26 15:18:37 +0700

    Layouting dan Children Routes (SPA)

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
+
 import Home from "../pages";
 import About from "../pages/About";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    // element: <Home />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
   {
     path: "/about",
